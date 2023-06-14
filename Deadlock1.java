29c2425a31f270d74d4c9618c0b4c4ef35a77407
21hr1a0453 <135335543+21hr1a0453@users.noreply.github.com>
2023-06-14 14:09:00 +0530

    Add files via upload

---
+++ Deadlock1.java
@@ -0,0 +1,34 @@
+class TestDeadlockExample1{
+public static void main(String args[]){
+final String resource1="ratan jaiswal";
+final String resource2="vimal jaiswal";
+Thread t1=new Thread(){
+public void run(){
+synchronized(resource1){
+System.out.println("Thread 1:locked resource1");
+try{ Thread.sleep(100);}
+catch(Exception e){}
+synchronized(resource2){
+System.out.println("Thread 1:locked resource2");
+}
+}
+}
+};
+Thread t2=new Thread(){
+public void run(){
+synchronized(resource2){
+System.out.println("Thread 2:locked resource2");
+try{ Thread.sleep(100);}
+catch(Exception e){}
+synchronized(resource1){
+System.out.println("Thread 2:locked resource1");
+}
+}
+}
+};
+t1.start();
+t2.start();
+}
+}
+
+
---
+++ InterThread2.java
@@ -0,0 +1,14 @@
+class TestInterruptingThread2 extends Thread{
+public void run(){
+try{
+Thread.sleep(1000);
+System.out.println("task");
+}catch(InterruptedException e){System.out.println("Exception handled"+e);}
+System.out.println("thread is runing....");
+}
+public static void main(String args[]){
+TestInterruptingThread2 t1=new TestInterruptingThread2();
+t1.start();
+t1.interrupt();
+}
+}
